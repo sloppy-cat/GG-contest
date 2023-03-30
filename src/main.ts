77aab9ba6fcfff17ba4be0902236e63f90df278a
psycode04 <psy.code04@gmail.com>
2023-03-30 10:59:19 +0900

    cdn

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 
+// import './style/bootstrap.css';
+// import './style/bootstrap-vue-3.css';
+
+import './style/reset.css';
+import './style/common.css';
+import './style/design.css';
+
 const app = createApp(App);
 // app.use(BootstrapVue3);
 app.use(router);
