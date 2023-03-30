8078021996d20852880e7949bfd60e64a8550c57
sloppy-cat <js3891129@gmail.com>
2023-03-30 13:41:40 +0900

    에러픽스

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,7 @@ import Axios from 'axios';
 import type { AxiosResponse, CancelTokenSource } from 'axios';
 import type { ReqBodyLoginUser } from './types/User';
 import type Notice from './types/Notice';
-import type {
-  ReqBodyCreateNotice,
-  ReqBodyEditNotice,
-  ResponseNoticeList,
-  ResponseNoticeDetail,
-} from './types/Notice';
+import type { ReqBodyCreateNotice, ReqBodyEditNotice, ResponseNoticeList } from './types/Notice';
 
 const dummyToken =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOiJiZWFzdEBrdC5jb20iLCJzdWIiOiJ1c2VyIiwiaWF0IjoxNjgwMTQ3NzEzLCJleHAiOjE2ODAxNjU3MTN9.F5HyeWNDvfXzC7HpRnL8FE4zV2xdKZe9doNRBlB1sjE--TjZ90ThrdkSAuRF_WsAZAgLsX_BNkQ4VIr3jMRBv10yxmMr-lFQIR6aWk7hmN81S4Mi--vlYskUNPXRwDVBmP7DJjiEAjrQNX3RzHI1vT3na-i3t7feHj6smH-Bgk0sQ2nNVcyNUj4PNgA9WEPlpwRBSesqxxoimZIcKsnWyOwVm9eqPSggJ-0bJcO2ERjUp0NdCOtfeOy8ktwEIHKQlLv2hzwIT4IuAkolvbP5nBjBzYgItI0NmP9c0sjv3rKYsellBDczaAh2YzsYHgNoefrIJsoaz2qQPf3lEgmMLA';
